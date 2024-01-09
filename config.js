64a1046439c22c05b5ea8bcaf2a9db699652b015
Aman Chourasia <82589551+amanChourasia1234@users.noreply.github.com>
2024-01-10 00:58:10 +0530

    Add files via upload

---
+++ config.js
@@ -0,0 +1,7 @@
+export default {
+  JWT_SECRET: 'TXNQzh7dckQ/Kb7WXhw2IaYI/cHd7y7VRezaLHeWOQs=',
+  EMAIL: 'abigayle.macgyver5@ethereal.email',
+  PASSWORD: '8bfmNEFbcGWUexHJJB',
+  ATLAS_URI:
+    'mongodb+srv://aman:aman12345@cluster0.0o16ygc.mongodb.net/?retryWrites=true&w=majority',
+};
