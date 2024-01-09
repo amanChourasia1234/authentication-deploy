e1a8e421cdddd327b5a1fe42b97b2287adc8415f
Aman Chourasia <82589551+amanChourasia1234@users.noreply.github.com>
2024-01-10 00:45:29 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from 'express';
 import cors from 'cors';
 import morgan from 'morgan';
 import connect from './database/connect.js';
-import router from '../router/route.js';
+import router from './router/route.js';
 
 import path from 'path';
 import { fileURLToPath } from 'url';
